31ec77697806b9281e28e1b1443115fbb745f43c
Robin Tijou <robintijou05@gmail.com>
2024-11-28 14:46:23 +0100

    ajout de la page creation d'article

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/articles/create', [UserController::class, 'create'])->name('articles.create');
+    Route::post('/articles/store', [UserController::class, 'store'])->name('articles.store');
 });
+
+
 
 require __DIR__.'/auth.php';
